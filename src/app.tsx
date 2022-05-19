9abd17b911c91852a53f1523ab11ecb71721a9e1
Thanh Dang <thanh.dang@bounteous.com>
2022-05-19 15:19:33 -0500

    Added all song data and filters for BITE5 tournament.  Removed Red Note song data

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ import { ThemeSyncWidget } from "./theme-toggle";
 export function App() {
   return (
     <ConfigStateManager>
-      <DrawStateManager defaultDataSet="1-single-swiss">
+      <DrawStateManager defaultDataSet="1-hard-14-16">
         <ThemeSyncWidget />
         <UpdateManager />
         <Header />
